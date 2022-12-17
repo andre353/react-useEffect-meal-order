2c14cf9da3b59a4e0fc33229e816190b437acf13
andre353 <eva595eva@gmail.com>
2022-12-17 19:01:14 +0300

    Use context for cart state management

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
+import { useState } from 'react';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true)
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false)
+  };
+
   return (
-    <>
-      <Cart />
-      <Header>Let's get started!</Header>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler}>Let's get started!</Header>
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 };
 
