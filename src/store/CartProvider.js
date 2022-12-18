1f0fd4305f8d7064268029881d35f4ed393bd579
andre353 <eva595eva@gmail.com>
2022-12-18 21:22:42 +0300

    Improve Reducer logic

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,8 +9,26 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
     // The concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
-    const updatedItems = state.items.concat(action.item); // update array in an immutable way
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+    
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem; // we pick the old array item and overwrite it with a new item
+    } else {
+      updatedItems = state.items.concat(action.item); // update array in an immutable way
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
