3435ea5c6245cbb635aa6482622776b05f4ce758
andre353 <eva595eva@gmail.com>
2022-12-18 22:20:27 +0300

    Add conditional animation class to button triggered by context items change

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,36 @@
-import {useContext} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext); // HeaderCartButton will be evaluated by react each time cartCtx gets updated in CartProvider
 
   // reduce is a built in method, allows to transform an array of data in a single value = number here
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
-  }, 0)
+  }, 0);
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer); // we need this clearTimeout function here in case of multiple clicks on Add button
+    };
+  }, [cartCtx.items]);
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
